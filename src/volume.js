cc3ac4d3f23b1396ba8e56c32ae3a0bdd0a4db50
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-07 12:26:05 +0000

    Arrgghhhhhh, perimeter's here

--- src/volume.js
+++ src/volume.js
@@ -24,4 +24,33 @@ class Volume {
   volumeCylinder(r, h) {
     return pi * sq(r) * h;
   }
+
+  // Perimeter
+  perimeterOfCircle(r) {
+    return 2 * pi * r;
+  }
+
+  perimeterOfRectangle(l, w) {
+    return 2 * (l + w);
+  }
+
+  perimeterOfSquare(l) {
+    return 4 * l;
+  }
+
+  perimeterOfTriangle(a, b, c) {
+    return a + b + c;
+  }
+
+  perimeterOfRegularPolygon(n, s) {
+    return n * s;
+  }
+
+  archimedianSpiral(a, angle) {
+    return a * angle;
+  }
+
+  arclength(r, angle) {
+    return r * angle;
+  }
 }
