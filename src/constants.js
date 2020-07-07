7938d0d403af54fc55e1b4b724ff1c06f698319c
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-07 13:37:29 +0000

    e over pi

--- src/constants.js
+++ src/constants.js
@@ -1,27 +1,24 @@
 module.exports = {
   pi: Math.PI,
-  sqrt: (num) => {
-    return Math.sqrt(num);
-  },
-  sq: (num) => {
-    return num ** 2;
-  },
-  cube: (num) => {
-    return Math.pow(num, 3);
-  },
-  qube: (num) => {
-    return Math.pow(num, 4);
-  },
-  sin: (ang) => {
-    return Math.sin(ang);
-  },
-  cos: (ang) => {
-    return Math.cos(ang);
-  },
-  tan: (ang) => {
-    return Math.tan(ang);
-  },
-  log: (num) => {
-    return Math.log(num);
-  },
+  e: Math.E,
+  lg10: Math.LN10,
+  lg2: Math.LN2,
+
+  sqrt: (num) => Math.sqrt(num),
+  cbrt: (num) => Math.cbrt(num),
+  sq: (num) => num ** 2,
+  cube: (num) => Math.pow(num, 3),
+  qube: (num) => Math.pow(num, 4),
+  sin: (ang) => Math.sin(ang),
+  cos: (ang) => Math.cos(ang),
+  tan: (ang) => Math.tan(ang),
+  sini: (ang) => Math.asin(ang),
+  cosi: (ang) => Math.acos(ang),
+  tani: (ang) => Math.atan(ang),
+  log: (num) => Math.log10(num),
+  log2: (num) => Math.log2(num),
+  loge: (num) => Math.log(num),
+  round: (num) => Math.round(num),
+  roundUp: (num) => Math.ceil(num),
+  roundDown: (num) => Math.floor(num),
 };
