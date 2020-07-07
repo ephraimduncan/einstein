815c74da647c318a296304fc7063b37a1ee5843d
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-07 12:18:21 +0000

    Archimedes Vs Euler

--- src/geometry.js
+++ src/geometry.js
@@ -31,7 +31,43 @@ class Geometry {
     return `${A}x + ${B}y + ${C}z + ${D}`;
   }
 
-  equationOfHyperboloidOfOneSheet() {}
+  equationOfHyperboloidOfOneSheet(x0, y0, z0, a, b, c) {
+    return `${sq(b) * sq(c)}(x - ${x0})² + ${sq(a) * sq(c)}(y - ${y0})² - ${
+      sq(b) * sq(a)
+    }(z - ${z0})² = ${sq(a) * sq(b) * sq(c)}`;
+  }
+
+  equationOfEllipticCone(x0, y0, z0, a, b, c) {
+    return `${sq(b) * sq(c)}(x - ${x0})² + ${sq(a) * sq(c)}(y - ${y0})² = ${
+      sq(b) * sq(a)
+    }(z - ${z0})²`;
+  }
+
+  equationOfEllipticCylinder(x0, y0, a, b) {
+    return this.equationOfEllipse(x0, y0, a, b);
+  }
+
+  equationOfHyperboloidOfTwoSheet(x0, y0, z0, a, b, c) {
+    return `${sq(b) * sq(c)}(x - ${x0})² - ${sq(a) * sq(c)}(y - ${y0})² - ${
+      sq(b) * sq(a)
+    }(z - ${z0})² = ${sq(a) * sq(b) * sq(c)}`;
+  }
+
+  equationOfEllipticParaboloid(x0, y0, z0, a, b, c) {
+    return `${sq(b) * sq(c)}(x - ${x0})² + ${sq(a) * sq(c)}(y - ${y0})² = ${
+      sq(b) * sq(a)
+    }(z - ${z0})`;
+  }
+
+  equationOfHyperbolicParaboloid(x0, y0, z0, a, b, c) {
+    return `${sq(b) * sq(c)}(x - ${x0})² - ${sq(a) * sq(c)}(y - ${y0})² = ${
+      sq(b) * sq(a)
+    }(z - ${z0})`;
+  }
+
+  equationOfParabola(x0, y0, a) {
+    return `(y - ${y0})² = ${4 * a}(x - ${x0})²`;
+  }
 
   distanceBetweenTwoPoints2D(x1, x2, y1, y2) {
     return sqrt(sq(x1 - x2) + sq(y1 - y2));
