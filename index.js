8836cfd11df3990f31d04561db01088779bb3b41
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-07 14:53:43 +0000

    Bugs, hit 'em

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const area = require('./src/maths/area');
-const chemistry = require('./src/area');
-const physics = require('./src/area');
-const geometry = require('./src/maths/area');
-const volume = require('./src/maths/area');
+const chemistry = require('./src/chemistry');
+const physics = require('./src/physics');
+const geometry = require('./src/maths/geometry');
+const volume = require('./src/maths/volume');
+
+class Formulae {}
